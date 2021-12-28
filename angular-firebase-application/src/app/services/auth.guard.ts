d8471497034b9cf0e0f1244665256e642c6be56a
ashwininingune <ashwini_ningune@persistent.com>
2021-12-28 18:56:48 +0530

    updated service files

--- angular-firebase-application/src/app/services/auth.guard.ts
+++ angular-firebase-application/src/app/services/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
 
@@ -7,16 +7,14 @@ import { AuthenticationService } from './authentication.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authSrv: AuthenticationService, private router: Router) { }
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if(this.authSrv.isLoggedIn){
-        return true;
-      }
-      this.router.navigate(['/login']);
-      return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if(this.authService.isLoggedIn) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
   }
   
 }
--- angular-firebase-application/src/app/services/authentication.service.ts
+++ angular-firebase-application/src/app/services/authentication.service.ts
@@ -1,27 +1,55 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 import { HotToastService } from '@ngneat/hot-toast';
+import { User } from '../models/user';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  userData: any;
   isLoggedIn: boolean = false;
 
-  constructor(private afAuth: AngularFireAuth, private toastService: HotToastService, private router: Router) { }
+  constructor(private afAuth: AngularFireAuth,
+              private afs: AngularFirestore, 
+              private toastService: HotToastService, 
+              private router: Router) {
+    this.afAuth.authState.subscribe(user => {
+      if(user) {
+        this.userData = user;
+        localStorage.setItem('User', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('User')!);
+      } else {
+        localStorage.setItem('User', 'null');
+        JSON.parse(localStorage.getItem('User')!);
+      }
+    })
+  }
+
+  setUserData(user: any) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      emailVerified: user.emailVerified
+    }
+    return userRef.set(userData, {merge: true})
+  }
 
   /* sign up */
   async signUp(email: string, password: string) {
     await this.afAuth.createUserWithEmailAndPassword(email, password)
-    .then(res => {
+    .then(result => {
       this.isLoggedIn = true;
-      console.log('You are Successfully Registered!', res);
+      this.setUserData(result.user);
+      console.log('You are Successfully Registered!', result);
       this.toastService.success('You are Successfully Registered!');
       this.router.navigate(['/login']);
-    })
-    .catch(error => {
+    }).catch(error => {
       console.log('Something is wrong: ', error.message);
       this.toastService.error('Something is wrong');
     });
@@ -30,22 +58,30 @@ export class AuthenticationService {
   /* sign in */
   async signIn(email: string, password: string) {
     await this.afAuth.signInWithEmailAndPassword(email, password)
-    .then(res => {
+    .then(result => {
       this.isLoggedIn = true;
-      console.log('You are login!');
+      this.setUserData(result.user);
+      console.log('You are login!', result);
       this.toastService.success('You are Successfully Login!');
       this.router.navigate(['/dashboard']);
-    })
-    .catch(err => {
+    }).catch(err => {
       console.log('Something went wrong: ', err.message);
       this.toastService.error('Student Not Found');
     });
   }
 
+  autoSignIn() {
+    const usersData = JSON.parse(localStorage.getItem('User')!);
+    console.log(usersData);
+    if(!usersData) {
+      return;
+    }
+  }
+
   /* sign out */
-  signOut() {
-    this.afAuth.signOut();
+  async signOut() {
+    await this.afAuth.signOut();
     localStorage.removeItem('User');
-    console.log('you are sign out');
+    this.router.navigate(['/']);
   }
 }
