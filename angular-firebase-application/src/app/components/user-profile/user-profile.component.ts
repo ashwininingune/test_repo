ab2e74e8a255516e9a117d1924f24fb7c8ea8519
ashwininingune <ashwini_ningune@persistent.com>
2021-12-28 18:16:26 +0530

    added new component

---
+++ angular-firebase-application/src/app/components/user-profile/user-profile.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+  user: any;
+  constructor(public authService: AuthenticationService) { }
+
+  ngOnInit(): void {
+  }
+
+}
